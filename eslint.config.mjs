ff19d162d743d1035b5f201c70c1ad3912cdccdc
Flunsi <60551481+Flunsi@users.noreply.github.com>
2024-11-11 20:36:59 +0100

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,15 @@ import tseslint from "typescript-eslint"
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{ files: ["**/*.{js,mjs,cjs,ts}"] },
-	{ ignores: ["dist"] },
 	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	{ ignores: ["dist"] },
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-unused-vars": "warn",
+			"prefer-const": "warn"
+		}
+	},
 ]
