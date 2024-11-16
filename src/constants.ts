91ce853f2387a8c194ed7c17f936000bc3403f2b
Flunsi <60551481+Flunsi@users.noreply.github.com>
2024-11-16 11:49:11 +0100

    constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,12 @@
-export const constants = {
-	INT1_MAX: 127,
-	INT1U_MAX: 255,
-	INT2_MAX: 32767,
-	INT2U_MAX: 65535,
-	INT4_MAX: 2147483647,
-	INT4U_MAX: 4294967295,
-	DB_MAX_SMALLINT: 32767,
-	DB_MAX_INT: 2147483647,
-} as const
+export const INT1_MAX = 127
+export const INT1U_MAX = 255
+export const INT2_MAX = 32767
+export const INT2U_MAX = 65535
+export const INT4_MAX = 2147483647
+export const INT4U_MAX = 4294967295
+
+export const DB_MAX_SMALLINT = 32767
+export const DB_MAX_INT = 2147483647
+
+export const DAY_TO_SEC = 86400
+export const DAY_TO_DECISEC = 864000
